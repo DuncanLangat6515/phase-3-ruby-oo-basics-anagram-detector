4992b7b3072bd3060f0cacc0b037857e601e9428
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-10-23 11:56:57 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    
+    def match(words)
+        words.filter do |word|
+           # words.select do |word|
+        word.split('').sort == @word.split('').sort
+        end
+    end
+end
